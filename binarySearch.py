55ca0a8b25fab4cccd081dd6c4d43ef064328d04
Zik <79639807+Isafatokun@users.noreply.github.com>
2023-05-16 15:47:33 +0200

    Create binarySearch.py
    A simple binary search implementation using recursion

---
+++ binarySearch.py
@@ -0,0 +1,45 @@
+tests = []
+tests.append({
+    'data': [0, 2, 3, 4, 5],
+    'value': 3,
+})
+
+tests.append({
+    'data': [],
+    'value': 0,
+})
+
+tests.append({
+    'data': [1,2,2,2,2,3,4,5,6],
+    'value': 2
+})
+
+tests.append({
+    'data': [1,2,2,2,2,4,5,6],
+    'value': 3
+})
+
+tests.append({
+    'data': [1,2,2,2,2,3,4,4,5,6,7],
+    'value': 7
+})
+
+def searchRecursively(data,value,low,high):
+    if low > high:
+        return -1
+    else:
+        mid = low + (high-low)//2
+        if value == data[mid]:
+            return mid
+        elif value > data[mid]:
+            return searchRecursively(data,value,mid + 1,high)
+        elif value < data[mid]:
+            return searchRecursively(data,value,low,mid -1)
+
+def searchBinary(data,value):
+    low,high = 0, len(data) - 1
+    return searchRecursively(data,value,low,high)
+
+for test in tests:
+    print(searchBinary(**test))
+    
